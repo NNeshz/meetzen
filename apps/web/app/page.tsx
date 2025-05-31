d6d10c17745629d101bcac12aefb5ee0a0cb0557
Neftali Hernandez <neftaligow01@gmail.com>
2025-05-31 10:55:11 -0600

    :bug: (Style) Fix style errors and schema color

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@meetzen/ui/src/components/button";
+
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Meetzen</h1>
+      <p className="text-muted-foreground">
+        Plataforma para gestionar citas de manera automatica.
+      </p>
+      <Button>Comenzar</Button>
+    </div>
+  );
 }
