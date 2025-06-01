7ce742570f55992de5e363564b18c71b1a63ee19
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-05-31 20:16:30 -0500

    fix: union dependencies and add command clean workspace

--- packages/api/src/eden/index.ts
+++ packages/api/src/eden/index.ts
@@ -1,5 +1,5 @@
 import type { Api as server } from "@meetzen/api/src";
-import { env } from "@meetzen/api/src/utils/envs";
 import { treaty } from "@elysiajs/eden";
 
-export const apiClient = treaty<server>(env.NEXT_PUBLIC_BACKEND_URL).api;
+export const createApiClient = (url: string) =>
+  treaty<server>(url).api;
